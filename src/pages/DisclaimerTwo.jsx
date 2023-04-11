8b4b45e93a54ac456dd2cd37fdca61ed6ff5b773
Lance Yu <lanceyu619@gmail.com>
2023-04-10 21:07:33 -0400

    change lorem ipsum

--- src/pages/DisclaimerTwo.jsx
+++ src/pages/DisclaimerTwo.jsx
@@ -6,6 +6,7 @@ function Disclaimer2({ onAccept }) {
     <div>
       <h1>Disclaimer 2</h1>
       <p>
+        Disclaimer 2
         This website is provided for informational purposes only. By continuing
         to use this website, you agree to be bound by the terms and conditions
         set forth in our <a href="/terms">Terms of Use</a>.
